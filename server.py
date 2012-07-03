306718d387a876d2042dea08fb4daed1cbd8eabf
Williston Hayes <wshayes@gmail.com>
2012-07-03 08:45:10 +0400

    Parametrized socket's port and ip addr

--- server.py
+++ server.py
@@ -3,22 +3,33 @@
 import socket, sys
 
 def main(args) :
+
+	IP_ADDR = "localhost"
+	IP_PORT = 12345
+
+	UX_SOCK = "mysock"
+
 	if len(args) == 1 :
 		# Assume INET sockets
-
-		start_ip_sock()
+		start_ip_sock(IP_ADDR, IP_PORT)
 
 	else :
-		# Use unix sockets
-		start_ux_sock()
+		if args[1] == "ipv4" or args[1] == "ip" :
+			if len(args) == 3 :
+				IP_PORT = int(args[2])
+			start_ip_sock(IP_ADDR, IP_PORT)
+
+		else :
+			# Use unix sockets
+			if len(args) == 3:
+				UX_SOCK = args[2]
+			start_ux_sock(UX_SOCK)
 
 
-def start_ux_sock() :
-	MY_SOCK = "mysock"
-
+def start_ux_sock(ux_sock) :
 	s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 
-	s.bind(MY_SOCK)
+	s.bind(ux_sock)
 
 	s.listen(1)
 
@@ -33,12 +44,10 @@ def start_ux_sock() :
 	conn.close()
 
 
-def start_ip_sock() :
-	SOCK_ADDR = "localhost"
-	SOCK_PORT = 12345
+def start_ip_sock(addr, port) :
 
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.bind((SOCK_ADDR, SOCK_PORT))
+	s.bind((addr, port))
 
 	s.listen(1)
 
