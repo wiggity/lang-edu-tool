0d1e665cb1174f9c7f49cf3955687d9aabe2b7e0
Williston Hayes <wshayes@gmail.com>
2012-07-03 08:37:57 +0400

    Have a simple workin version..supports unix socks and ipv4 socks

--- server.py
+++ server.py
@@ -1,19 +1,56 @@
-import socket
+#!/usr/bin/python
 
-MY_SOCK = "mysock"
+import socket, sys
 
-s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+def main(args) :
+	if len(args) == 1 :
+		# Assume INET sockets
 
-s.bind(MY_SOCK)
+		start_ip_sock()
 
-s.listen(1)
+	else :
+		# Use unix sockets
+		start_ux_sock()
 
-conn, addr = s.accept()
-print 'Connected by', addr
-while 1:
-	data = conn.recv(1024)
-	if not data: 
-		break
-	print data
-	conn.send(data)
-conn.close()
+
+def start_ux_sock() :
+	MY_SOCK = "mysock"
+
+	s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+	s.bind(MY_SOCK)
+
+	s.listen(1)
+
+	conn, addr = s.accept()
+	print 'Connected by', addr
+	while 1:
+		data = conn.recv(1024)
+		if not data: 
+			break
+		print data
+		conn.send(data)
+	conn.close()
+
+
+def start_ip_sock() :
+	SOCK_ADDR = "localhost"
+	SOCK_PORT = 12345
+
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind((SOCK_ADDR, SOCK_PORT))
+
+	s.listen(1)
+
+	conn, addr = s.accept()
+	print 'Connected by', addr
+	while 1:
+		data = conn.recv(1024)
+		if not data: 
+			break
+		print data
+		conn.send(data)
+	conn.close()
+
+if __name__ == "__main__" :
+	main(sys.argv)
